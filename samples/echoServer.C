e9a168fcef50696b02543d542c54b874fc80fa51
Aniban Kundu <akundu@apache.org>
2010-05-07 21:19:22 +0000

    sample echoServer code using the server framework
    git-svn-id: https://svn.apache.org/repos/asf/trafficserver/traffic/branches/UserFiber@942234 13f79535-47bb-0310-9956-ffa450edef68

---
+++ samples/echoServer.C
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <stdlib.h>
+
+#include "UFIO.H"
+#include "UFServer.H"
+
+using namespace std;
+
+struct EchoServer : public UFServer
+{
+    EchoServer(char* interfaceIP, unsigned int port)
+    {
+        _addressToBindTo = interfaceIP ? interfaceIP : "";
+        _port = port;
+    }
+    void handleNewConnection(UFIO* ufio);
+};
+
+unsigned long long int readTimeout = 0;
+void EchoServer::handleNewConnection(UFIO* ufio)
+{
+    if(!ufio)
+    {
+        cerr<<"couldnt create UFIO object"<<endl;
+        return;
+    }
+
+    char buf[256];
+    while ( ((n = ufio->read(buf, 255, readTimeout)) > 0) && ((n = ufio->write(buf, 255)) > 0) ) {}
+}
+
+
+int main(int argc, char** argv)
+{
+    unsigned int numThreads = 8;
+    unsigned int numProcesses = 1;
+    unsigned short int port = 8080;
+    if(argc > 1)
+        numThreads = atoi(argv[1]);
+    if(argc > 2)
+        port = atoi(argv[2]);
+    if(argc > 3)
+        readTimeout = atoi(argv[3]);
+    if(argc > 4)
+        numProcesses = atoi(argv[4]);
+
+    EchoServer ufecho(0, port);
+    ufecho.MAX_ACCEPT_THREADS_ALLOWED   = 1;
+    ufecho.MAX_THREADS_ALLOWED          = numThreads;
+    ufecho.MAX_PROCESSES_ALLOWED        = numProcesses;
+    ufecho.UF_STACK_SIZE                = 8192;
+
+    ufecho.run();
+
+    return 0;
+}
